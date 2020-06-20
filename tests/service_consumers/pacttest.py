e9fa8f2ab7a02ad2cac785d137826016e1c67d5a
amrithajayadev <amrithajayadev@gmail.com>
2020-06-20 23:55:05 +0530

    Update pacttest.py

--- tests/service_consumers/pacttest.py
+++ tests/service_consumers/pacttest.py
@@ -2,12 +2,13 @@ from pact_test import ServiceConsumerTest, honours_pact_with, pact_uri, state
 
 from service import producerService
 
-broker_url = 'https://ciqpact.cec.lab.emc.com:8443/pacts/provider/{provider}/consumer/{consumer}/version/{consumerApplicationVersion}'.format(
+host = 'https://pact.broker.url.domain.com:8443'
+broker_url = '{host}/pacts/provider/{provider}/consumer/{consumer}/version/{consumerApplicationVersion}'.format(host=host,
     provider='pipeline-state-manager', consumer='consumer', consumerApplicationVersion='1.0.0')
 
 
 @honours_pact_with('consumer')
-@pact_uri('https://ciqpact.cec.lab.emc.com:8443/pacts/provider/python-producer/consumer/consumer/consumer'
+@pact_uri('https://pact.broker.url.domain.com:8443/pacts/provider/python-producer/consumer/consumer/consumer'
           '-python-producer.json')
 class ConsumerTest(ServiceConsumerTest):
     @state('inventory exists')
